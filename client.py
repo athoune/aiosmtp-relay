25315bda89dd73e88cf826ea0c76917ea3b81440
Mathieu Lecarme <mlecarme@bearstech.com>
2018-01-29 23:07:54 +0100

    Just a relay

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 from smtplib import SMTP as Client
 
 
-client = Client('127.0.0.1', 8025)
+client = Client('127.0.0.1', 2025)
 r = client.sendmail('a@example.com', ['b@example.com'], """\
 From: Anne Person <anne@example.com>
 To: Bart Person <bart@example.com>
--- relay.py
+++ relay.py
@@ -7,9 +7,22 @@ from aiosmtplib import SMTP as Client
 
 class RelayHandler:
 
+
+    def __init__(self, hostname="127.0.0.1", port=1025):
+        self.output_hostname = hostname
+        self.output_port = port
+
     async def handle_DATA(self, server, session, envelope):
         print(envelope)
         print(dir(envelope))
+        print(envelope.mail_options)
+        print(envelope.rcpt_options)
+        print(envelope.rcpt_tos)
+        c = Client()
+        await c.connect(hostname=self.output_hostname,
+                        port=self.output_port)
+        print(type(envelope.content))
+        await c.sendmail(envelope.mail_from, envelope.rcpt_tos, envelope.content )
         return '250 Message accepted for delivery'
 
 
@@ -17,6 +30,6 @@ if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     controller = Controller(RelayHandler(), loop=loop)
     print('listening')
-    server = loop.create_server(controller.factory, host='0.0.0.0', port=8025)
+    server = loop.create_server(controller.factory, host='0.0.0.0', port=2025)
     loop.run_until_complete(server)
     loop.run_forever()
