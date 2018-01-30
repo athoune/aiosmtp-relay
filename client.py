a41cc07a4ab7a5a4978495aa74a5e38c03681b55
Mathieu Lecarme <mlecarme@bearstech.com>
2018-01-30 23:03:51 +0100

    testing AUTH

--- client.py
+++ client.py
@@ -2,6 +2,8 @@ from smtplib import SMTP as Client
 
 
 client = Client('127.0.0.1', 2025)
+client.ehlo_or_helo_if_needed()
+client.login('bob', 'sponge')
 r = client.sendmail('a@example.com', ['b@example.com'], """\
 From: Anne Person <anne@example.com>
 To: Bart Person <bart@example.com>
--- relay.py
+++ relay.py
@@ -1,12 +1,11 @@
 import asyncio
-import logging
 
 from aiosmtpd.controller import Controller
 from aiosmtplib import SMTP as Client
+from aiosmtpd.smtp import SMTP
 
 
 class RelayHandler:
-
 
     def __init__(self, hostname="127.0.0.1", port=1025):
         self.output_hostname = hostname
@@ -22,14 +21,31 @@ class RelayHandler:
         await c.connect(hostname=self.output_hostname,
                         port=self.output_port)
         print(type(envelope.content))
-        await c.sendmail(envelope.mail_from, envelope.rcpt_tos, envelope.content )
+        await c.sendmail(envelope.mail_from, envelope.rcpt_tos,
+                         envelope.content)
         return '250 Message accepted for delivery'
+
+    #async def handle_AUTH(self, server, session, envelope, args):
+        #print(args)
+
+
+def auth(login, password):
+    print(login, password)
+    return True
+
+
+class RelayController(Controller):
+
+    def factory(self):
+        return SMTP(self.handler, decode_data=True, auth_required=True,
+                    auth_require_tls=False, auth_method=auth)
 
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    controller = Controller(RelayHandler(), loop=loop)
+    controller = RelayController(RelayHandler(), loop=loop)
     print('listening')
-    server = loop.create_server(controller.factory, host='0.0.0.0', port=2025)
+    server = loop.create_server(controller.factory, host='0.0.0.0',
+                                port=2025)
     loop.run_until_complete(server)
     loop.run_forever()
